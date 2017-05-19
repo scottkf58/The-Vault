445f76551fa89ca1e45db1a43bbd096e1a0b3b74
Scott Fujimoto <scottfujimoto@Scotts-MacBook-Pro.local>
2017-05-18 20:22:42 -1000

    access granted

--- vault.js
+++ vault.js
@@ -1,4 +1,23 @@
 'use strict';
-module.exports = function() {
-  
+ module.exports = function () {
+  var vault = {};
+
+  function setValue (key, value) {
+    vault.key = key;
+    vault.value = value;
+  }
+
+  function getValue (key) {
+    if(vault.key){
+      return vault.value;
+    }else{
+      return null;
+    }
+  }
+  return {
+    setValue:setValue,
+    getValue:getValue,
+  };
+
 };
+
